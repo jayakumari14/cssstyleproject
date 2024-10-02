c104410956be2a7af7ec27984a0be8e8a0c9d130
jayak <kumarijaya127@gmail.com>
2024-10-02 19:51:52 +0530

    map and filter added

--- script.js
+++ script.js
@@ -9,15 +9,37 @@
 //   console.log("not eligible");
 // }
 
-var fun = function app() {
-  console.log("i am funny app");
-  return;
-};
-var a = fun();
-console.log(a);
+// var fun = function app() {
+//   console.log("i am funny app");
+//   return;
+// };
+// var a = fun();
+// console.log(a);
 
-var arr = [10, 20, 30, 40, 50];
+// var arr = [10, 20, 30, 40, 50];
 
-arr.forEach((elm) => {
-  console.log("hello", elm);
+// arr.forEach((elm) => {
+//   console.log("hello", elm);
+// });
+
+const arr = ["jaya", "goutham", "piyush", "sam", "krishna"];
+// arr.forEach(function (val) {
+//   if (val === "sam") {
+//   } else {
+//     console.log(val);
+//   }
+// });
+
+// let newarr = arr.map((val) => {
+//   return val + " " + "ji";
+// });
+
+// console.log(newarr);
+
+const newarr = arr.filter(async (val) => {
+  if (val === "jaya") {
+    return true;
+  }
 });
+
+console.log(await newarr);
